0a436e01b6ab7573c0cf9dda50904bda5dce8988
KrazyDog13 <LKeane13@gmail.com>
2017-01-24 10:43:23 -0800

    Turtle

--- Turtle.py
+++ Turtle.py
@@ -1,1 +1,24 @@
 """ Create a Turtle, name it, make it BLUE and draw a Smiley Face"""
+import turtle
+wn = turtle.Screen()
+Logan Smile = turtle.Turtle()
+Logan Smile.color("Blue")
+
+Logan Smile.penup()
+Logan Smile.goto(-75,150)
+Logan Smile.pendown()
+Logan Smile.circle(10)
+
+Logan Smile.penup()
+Logan Smile.goto(75,150)
+Logan Smile.pendown()
+Logan Smile.circle(10)
+Logan Smile.penup()
+Logan Smile.goto(0,0)
+Logan Smile.pendown()
+Logan Smile.circle(100,90)
+
+Logan Smile.penup()
+Logan Smile.goto(0,0)
+Logan Smile.pendown()
+Logan Smile.circle(-100,90)
