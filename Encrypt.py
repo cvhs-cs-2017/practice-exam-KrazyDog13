6fb5f5cb7e10908923e0c9b7d051f66937dabf7f
KrazyDog13 <LKeane13@gmail.com>
2017-01-25 10:52:48 -0800

    fixed encrypt

--- Encrypt.py
+++ Encrypt.py
@@ -1,5 +1,5 @@
 """Write a code that will take (AnyString) and substitute all vowels with
 character's like $#%^&* etc..... but DO NOT USE ' or " as any of your characters."""
-def resource(AnyString):
+def removeVowels(AnyString):
     trans = str.maketrans('','','aeiouAEIOU')
     return string[0] + string[1:].translate(trans)
